b250940a2b4358fa49b2f224fac902450d29b00c
Swamy <swamy.dvps@gmail.com>
2021-05-16 15:31:06 +0530

    Commit 02

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you";
+    String someone = "hi World, Thanks you";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
